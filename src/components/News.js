c6c8e9710469ca443c8dc367d92fb82dff555f36
Mihika Saraf <mihikaps003@gmail.com>
2024-07-26 01:18:44 +0530

    News fetched from the API

--- src/components/News.js
+++ src/components/News.js
@@ -2,14 +2,45 @@ import React, { Component } from 'react'
 import NewsElement from './NewsElement'
 
 export class News extends Component {
+  constructor(){
+    super();
+   //console.log("hello i'm a constructor from news component");
+    this.state = {
+      articles : [],
+      loading :  false
+    }
+   }
+
+   async componentDidMount(){
+    //console.log("cdm");
+    let url = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=c8ac59d1aac74c8b96b814fd08d8c8d4";
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({articles: parsedData.articles})
+   }
+
+
   render() {
     return (
-      <div>
-        This is news component.
-        <NewsElement/>
+      <div className='container my-3'>
+        <h2>NewsPool Top Headlines</h2>
+        <div className="row">
+        {this.state.articles.map((element)=>{
+          return <div className="col-md-4" key={element.url}>
+          <NewsElement title={element.title?element.title:""} 
+          description={element.description?element.description:""} 
+          imageUrl={element.urlToImage} newsUrl={element.url}/>
+          </div>
+        })}
+          
+        </div>
+        
       </div>
     )
   }
 }
 
 export default News
+
+
--- src/components/NewsElement.js
+++ src/components/NewsElement.js
@@ -1,13 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class NewsElement extends Component {
+
   render() {
+    let {title, description, imageUrl, newsUrl} = this.props;
     return (
-      <div>
-        This is news element!
+      <div className="my-3">
+        <div className="card" style={{width:"20rem"}}>
+          <img src={imageUrl?imageUrl:"https://i.pinimg.com/originals/58/13/bb/5813bb127dda2b7f283247caf059d837.jpg"} className="card-img-top" alt="..." />
+          <div className="card-body d-flex flex-column">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">
+              {description}
+            </p>
+            <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">
+              Read More
+            </a>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default NewsElement
+export default NewsElement;
